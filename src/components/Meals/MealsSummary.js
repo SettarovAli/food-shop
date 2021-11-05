c37f9ac3ed1962fd851f0b74761528158491d07e
Settarov Ali <settarov.ali@gmail.com>
2021-11-05 20:54:38 +0200

    Add Meals, MealsAvailable, MealsSummary, MealItem, MealItemForm, Card, Input

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,23 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious food delivered to you</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore culpa
+        id distinctio officiis odio ad quaerat placeat, labore consequuntur
+        porro rem cum. Dolorum iure asperiores earum beatae odio reiciendis
+        fugit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore culpa
+        id distinctio officiis odio ad quaerat placeat, labore consequuntur
+        porro rem cum. Dolorum iure asperiores earum beatae odio reiciendis
+        fugit.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
